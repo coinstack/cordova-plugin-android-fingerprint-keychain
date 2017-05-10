b16a243aa9cb7474fd351ca78bb96049ca5e54f2
leejinhwan <dilcheck@blocko.io>
2017-05-10 20:14:54 +0900

    remake form

--- src/android/FingerprintUiHelper.java
+++ src/android/FingerprintUiHelper.java
@@ -187,7 +187,7 @@ public class FingerprintUiHelper extends FingerprintManagerCompat.Authentication
         
         switch (errCode) {
             case 7: 
-                mErrorTextView.setText(locale.tooManyTries);
+                mErrorTextView.setText("지문인식에 실패하였습니다. 잠시후에 다시 시도해주세요");
                 break;
             default:
                 mErrorTextView.setText(error);
@@ -210,7 +210,7 @@ public class FingerprintUiHelper extends FingerprintManagerCompat.Authentication
             int fingerprint_hint_id = mContext.getResources()
                     .getIdentifier("fingerprint_hint", "string", FingerprintScanner.packageName);
             if (locale != null) {
-                mErrorTextView.setText(locale.hintText);
+                mErrorTextView.setText("지문인식을 하여 개인인증을 해주세요");
             } else {
                 mErrorTextView.setText(
                         mErrorTextView.getResources().getString(fingerprint_hint_id));
